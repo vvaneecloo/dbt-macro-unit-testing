2ee959ad9ebc80caa92e6709bb2f57f29be186a2
victorvaneecloo <victor.vaneecloo3@gmail.com>
2025-07-23 18:43:18 +0200

    add unit tests for assertGreater / Lesser

--- macros/assertGreater_Lesser/unit_test__assertGreater_Lesser.sql
+++ macros/assertGreater_Lesser/unit_test__assertGreater_Lesser.sql
@@ -7,19 +7,25 @@
 {% endmacro %}
 
 {% macro unit_test__assertLesser() %}
-    {% do assertNotEqual(1,  add(1, 1), "1", is_query=True) %}
-    {% do assertNotEqual(2,  add(1, -1), "1", is_query=True) %}
-    {% do assertNotEqual(3,  add(1, -2), "1", is_query=True) %}
-    {% do assertNotEqual(4,  multiply(1, 1), "2", is_query=True) %}
-    {% do assertNotEqual(5,  multiply(1, -1), "3", is_query=True) %}
-    {% do assertNotEqual(6,  multiply(2, -2), "4", is_query=True) %}
-    {% do assertNotEqual(7,  not_casted_power(1, 1), "3", is_query=True) %}
-    {% do assertNotEqual(8,  not_casted_power(1, -1), "45", is_query=True) %}
-    {% do assertNotEqual(9,  not_casted_power(-2, 2), "6", is_query=True) %}
-    {% do assertNotEqual(10,  power(1, 1), "2", is_query=True) %}
-    {% do assertNotEqual(11,  power(1, -1), "3", is_query=True) %}
-    {% do assertNotEqual(12,  power(-2, 2), "5", is_query=True) %}
-    {% do assertNotEqual(10, add_days_to_date("2025-07-21", 1), "2025-07-23") %}
-    {% do assertNotEqual(11, add_days_to_date("2025-07-21", -1), "2025-07-21") %}
-    {% do assertNotEqual(12, add_days_to_date("2025-07-21", 2), "2025-07-22") %}
+    {% do assertLesser(1,  add(1, 1), "3", is_query=True) %}
+    {% do assertLesser(2,  multiply(1, 1), "2", is_query=True) %}
+    {% do assertLesser(3,  not_casted_power(1, 1), "2", is_query=True) %}
+    {% do assertLesser(4,  power(1, 1), "2", is_query=True) %}
+    {% do assertLesser(5, add_days_to_date("2025-07-21", 1), "2025-07-23") %}
+{% endmacro %}
+
+{% macro unit_test__assertGreatEqual() %}
+    {% do assertGreatEqual(1,  add(1, 1), "0", is_query=True) %}
+    {% do assertGreatEqual(2,  multiply(1, 1), "1", is_query=True) %}
+    {% do assertGreatEqual(3,  not_casted_power(1, 1), "1", is_query=True) %}
+    {% do assertGreatEqual(4,  power(1, 1), "1", is_query=True) %}
+    {% do assertGreatEqual(5, add_days_to_date("2025-07-21", 1), "2025-07-22") %}
+{% endmacro %}
+
+{% macro unit_test__assertLessEqual() %}
+    {% do assertLessEqual(1,  add(1, 1), "3", is_query=True) %}
+    {% do assertLessEqual(2,  multiply(1, 1), "2", is_query=True) %}
+    {% do assertLessEqual(3,  not_casted_power(1, 1), "2", is_query=True) %}
+    {% do assertLessEqual(4,  power(1, 1), "2", is_query=True) %}
+    {% do assertLessEqual(5, add_days_to_date("2025-07-21", 1), "2025-07-23") %}
 {% endmacro %}
