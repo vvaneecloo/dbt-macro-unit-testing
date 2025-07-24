6789b8d5dbd3adbba9368731022344b4f611a231
victorvaneecloo <victor.vaneecloo3@gmail.com>
2025-07-24 22:10:38 +0200

    fix In operator logic

--- macros/assertIn/unit_test_assertIn.sql
+++ macros/assertIn/unit_test_assertIn.sql
@@ -1,9 +1,9 @@
 {% macro unit_test__assertIn() %}
-    {% do assertIn(1, add(1, 1), [2, 3, 4], is_query=True) %}
-    {% do assertIn(2, multiply(2, 3), [6, 12, 18], is_query=True) %}
+    {% do assertIn(1, add(1, 1), ['2', 3, 4], is_query=True) %}
+    {% do assertIn(2, multiply(2, 3), ['6', 12, 18], is_query=True) %}
     {% do assertIn(3, not_casted_power(2, 2), ['4', '4.0', 4], is_query=True) %}
-    {% do assertIn(4, power(3, 2), [9, 9.0], is_query=True) %}
-    {% do assertIn(5, add_days_to_date("2025-07-21", 1), "2025", is_query=False) %}
+    {% do assertIn(4, power(3, 2), ['9', 9.0], is_query=True) %}
+    {% do assertIn(5, "2025-07-21", ['1997'], is_query=True) %}
 {% endmacro %}
 
 {% macro unit_test__assertNotIn() %}
